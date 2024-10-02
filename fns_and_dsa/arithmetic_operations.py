fb7631bfe63a32951131447e0ade5fe589d23753
jeddy2023 <jkenosuh@gmail.com>
2024-10-02 09:23:03 +0100

    Function task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,17 @@
 # Define the perform_operation function
 def perform_operation(num1, num2, operation):
     # Perform the arithmetic operation based on the provided operation parameter
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            # Handle division by zero
-            if num2 == 0:
-                return "Cannot divide by zero"
-            else:
-                return num1 / num2
-        case _:
-            return "Invalid operation"
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        # Handle division by zero
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Invalid operation"
